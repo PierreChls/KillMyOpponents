eac63d18a84ce6e034643dfa41e3bc452e2722e5
Pierre Charles <pierre.chls@gmail.com>
2016-01-28 20:08:25 +0100

    Add configuration auth twitter file
    Update App.js with auth_twitter information, add gitignore to ignore node_moduls folder and my configuration file to connect my twitter app

--- app.js
+++ app.js
@@ -1,38 +1,63 @@
 var app = require('http').createServer(handler)
-    , io = require('socket.io').listen(app)
-    , fs = require('fs')
-    , twitter = require('ntwitter')
-    , util = require('util');
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
+  , twitter = require('ntwitter')
+  , path = require('path')
+  , config = require('./init/config')
+  , auth = require('./init/twitter_auth');
    
    
  //INIT TWITTER
  var twit = new twitter({
-    consumer_key: '[YOUR_CONSUMER_KEY]',
-    consumer_secret: '[YOUR_CONSUMER_SECRET]',
-    access_token_key: '[YOUR_ACCESS_TOKEN_KEY]',
-    access_token_secret:'[YOUR_ACCESS_TOKEN_SECRET]'
+    consumer_key: auth.consumer_key,
+    consumer_secret: auth.consumer_secret,
+    access_token_key: auth.access_token_key,
+    access_token_secret: auth.access_token_secret
  });
   
   
-  app.listen(8080);
+app.listen(8080);
+
 function handler (req, res) {
-	fs.readFile(__dirname + '/index.html',
- 	function (err, data) {
- 		if (err) {
-        	res.writeHead(500);
-        	return res.end('Error loading index.html');
-      	}
+  var filePath = '.' + req.url;
+  if (filePath == './') filePath = './view/index.html';
+    
+  var extname = path.extname(filePath);
+  var contentType = 'text/html';
+  switch (extname) {
+    case '.js':
+    contentType = 'text/javascript';
+  break;
+  case '.css':
+    contentType = 'text/css';
+  break;
+  }
   
-      	res.writeHead(200);
-     	res.end(data);
-	});
+  fs.exists(filePath, function(exists) {
+    if (exists) {
+      fs.readFile(filePath, function(err, data) {
+        if (err) {
+          res.writeHead(500);
+          res.end();
+        }
+        else {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(data, 'utf-8');
+        }
+      });
+     }
+     else {
+       res.writeHead(404);
+       res.end();
+     }
+  });
 }
   
- io.sockets.on('connection', function(socket) {
- 	twit.stream('statuses/filter', {'track': 'stagiaire'},
+io.sockets.on('connection', function(socket) {
+ 	twit.stream('statuses/filter', {'track': config.hashtag},
 		function(stream) {
 			stream.on('data',function(data){
 			socket.emit('twitter',data);
 		});
 	});
- });
+});
